8e707c661a33cc804f030de6e0f6822088e029f1
momentspace <service@starferry.net>
2014-07-19 14:04:17 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WercherTest::Application.config.secret_token = '734202e0099b7ddb5e626827e1a0b2f3bbe0eeb4b22133190cb398223fb95a46a59255fb097e1130023694efaf6aeffdca68466022af4aee67589c2f00734199'
